eec2b98d5e55ed055c84adf766cf6f7548f80417
Anil kumar <anil.k@greyorange.com>
2020-04-30 20:31:11 +0530

    BSS-22115: replacing scan option with select option in select mtu page

--- src/assets/js/components/SplitPPS.js
+++ src/assets/js/components/SplitPPS.js
@@ -1,20 +1,27 @@
 var React = require('react');
 var allresourceConstants = require('../constants/resourceConstants');
+var mainstore = require("../stores/mainstore")
+var CommonActions = require("../actions/CommonActions")
 
 var SplitPPS = React.createClass({
+
+
+	onMtuSelect: function(mtuId){
+		var currentStationId = mainstore.getCurrentStationId();
+		var data = {
+            "name": "scanned",
+            "data": mtuId,
+            "screen_id": mainstore.getScreenId()
+          }
+          CommonActions.postDataToInterface(data, currentStationId)
+	},
 
 	processData: function () {
 		let groupInfo = this.props.groupInfo && this.props.groupInfo.ppsBinIds ? this.props.groupInfo.ppsBinIds : this.props.groupInfo;
 		var data = Object.assign({}, (groupInfo || {}));
 		var binColors = Object.assign({}, (this.props.groupInfo.ppsBinIdColors || {}));
 		var leftCol = [],
-			// dockedGroup = this.props.docked || [],
-			// undockAwaited = this.props.undockAwaited || [],
-			// printReady = this.props.printReady || [],
-			// wrongUndock = this.props.wrongUndock || [],
-			// selectedBin = this.props.selectedbin || [],
 			rightCol = [], centerCol = [], maxBlockCount = 0, maxLeftCount = 0, maxRightCount = 0, maxBlockHeight = 0, maxCenterCount = 0, style = null, maxWidth = 0;
-		//dockedGroup = dockedGroup.filter(val => !selectedBin.includes(val));
 		for (var key in data) {
 			if (data[key] === allresourceConstants.BIN_GROUP_LEFT) {
 				maxLeftCount++;
@@ -50,121 +57,12 @@ var SplitPPS = React.createClass({
 				'padding': padding
 			})
 		}
-		// var dockedclassName = this.props.ruleset === 'withBorder' ? "dockedCont bottomBorderLeft" : "dockedCont";
-		// var undockclassName = this.props.ruleset === 'withBorder' ? "undockedCont bottomBorderLeft" : "undockedCont";
-		// var printReadyclassName = this.props.ruleset === 'withBorder' ? "printReadyCont bottomBorderLeft" : "printReadyCont";
-		// var wrongUndockclassName = this.props.ruleset === 'withBorder' ? "wrongUndockCont bottomBorderLeft" : "wrongUndockCont";
-		// var selectedbinclassName = this.props.ruleset === 'withBorder' ? "selectedbinCont bottomBorderLeft" : "selectedbin";
-
-		// var dockedRightclassName = this.props.ruleset === 'withBorder' ? "dockedCont bottomBorderRight" : "dockedCont";
-		// var undockRigtclassName = this.props.ruleset === 'withBorder' ? "undockedCont bottomBorderRight" : "undockedCont";
-		// var printReadyRigtclassName = this.props.ruleset === 'withBorder' ? "printReadyCont bottomBorderRight" : "printReadyCont";
-		// var wrongUndockRightclassName = this.props.ruleset === 'withBorder' ? "wrongUndockCont bottomBorderRight" : "wrongUndockCont";
-		// var selectedbinRightclassName = this.props.ruleset === 'withBorder' ? "selectedbinCont bottomBorderRight" : "selectedbin";
 
 		for (var k in data) {
 			if (data.hasOwnProperty(k)) {
-
-				// if (data[k] === allresourceConstants.BIN_GROUP_LEFT) {
-
-				// 	if (dockedGroup.indexOf(k) >= 0) {
-				// 		leftCol.push(<li key={k} style={style} className={dockedclassName}>
-				// 			<span className={this.props.ruleset === 'withBorder' ? "" : "docked"}>{this.props.displayBinId ? k : null}</span>
-				// 		</li>);
-				// 	}
-				// 	else if (undockAwaited.indexOf(k) >= 0) {
-				// 		leftCol.push(<li key={k} style={style} className={undockclassName}>
-				// 			<span style={{ backgroundColor: binColors[k] }} className="undock left">&nbsp;</span>
-				// 		</li>);
-				// 	}
-				// 	else if (printReady.indexOf(k) >= 0) {
-				// 		leftCol.push(<li key={k} style={style} className={printReadyclassName}>
-				// 			<span className="printReady left">&nbsp;</span>
-				// 		</li>);
-				// 	}
-				// 	else if (wrongUndock.indexOf(k) >= 0) {
-				// 		leftCol.push(<li key={k} style={style} className={wrongUndockclassName}>
-				// 			<span className="wrongUndock left">{this.props.displayBinId ? k : null}</span>
-				// 		</li>);
-				// 	}
-				// 	else if (selectedBin.indexOf(k) >= 0) {
-				// 		leftCol.push(<li key={k} style={style} className={selectedbinclassName}>
-				// 			<span className="selectedbin">{this.props.displayBinId ? k : null}</span>
-				// 		</li>);
-				// 	}
-				// 	else {
-				// 		leftCol.push(<li key={k} style={style} className={this.props.ruleset === 'withBorder' ? "bottomBorderLeft padding noBackGround" : "padding noBackGround"} ><span>{this.props.displayBinId ? k : null}</span></li>);
-				// 	}
-
-				// }
-				// else if (data[k] === allresourceConstants.BIN_GROUP_RIGHT) {
-				// 	if (dockedGroup.indexOf(k) >= 0) {
-				// 		rightCol.push(<li key={k} style={style} className={dockedRightclassName}>
-				// 			<span className={this.props.ruleset === 'withBorder' ? "" : "docked"}>{this.props.displayBinId ? k : null}</span>
-				// 		</li>);
-				// 	}
-				// 	else if (undockAwaited.indexOf(k) >= 0) {
-				// 		rightCol.push(<li key={k} style={style} className={undockRigtclassName}>
-				// 			<span style={{ backgroundColor: binColors[k] }} className="undock right">&nbsp;</span>
-				// 		</li>);
-				// 	}
-				// 	else if (printReady.indexOf(k) >= 0) {
-				// 		rightCol.push(<li key={k} style={style} className={printReadyRigtclassName}>
-				// 			<span className="printReady right">&nbsp;</span>
-				// 		</li>);
-				// 	} else if (wrongUndock.indexOf(k) >= 0) {
-				// 		rightCol.push(<li key={k} style={style} className={wrongUndockRightclassName}>
-				// 			<span className="wrongUndock right">{this.props.displayBinId ? k : null}</span>
-				// 		</li>);
-				// 	}
-				// 	else if (selectedBin.indexOf(k) >= 0) {
-				// 		rightCol.push(<li key={k} style={style} className={selectedbinRightclassName}>
-				// 			<span className="selectedbin">{this.props.displayBinId ? k : null}</span>
-				// 		</li>);
-				// 	}
-				// 	else {
-				// 		rightCol.push(<li key={k} style={style} className={this.props.ruleset === 'withBorder' ? "bottomBorderRight padding noBackGround" : "padding noBackGround"} ><span>{this.props.displayBinId ? k : null}</span></li>);
-				// 	}
-
-				// }
-				//else 
 				if (data[k] === allresourceConstants.BIN_GROUP_CENTER || data[k] === allresourceConstants.BIN_GROUP_CENTER_TOP) {
-					// if (dockedGroup.indexOf(k) >= 0) {
-					// 	centerCol.push(<li key={k} style={style} className="dockedCont">
-					// 		<span className={this.props.ruleset === 'withBorder' ? "" : "docked"}>{this.props.displayBinId ? k : null}</span>
-					// 	</li>);
-					// }
-					// else if (undockAwaited.indexOf(k) >= 0) {
-					// 	centerCol.push(<li key={k} style={style} className="undockedCont">
-					// 		<span >{this.props.displayBinId ? k : null}</span>
-					// 		<span style={{ backgroundColor: binColors[k] }} className="undock below">
-					// 		</span>
-					// 	</li>);
-					// }
-					// else if (printReady.indexOf(k) >= 0) {
-					// 	centerCol.push(<li key={k} style={style} className="printReadyCont">
-					// 		<span >{this.props.displayBinId ? k : null}</span>
-					// 		<span className="printReady below">
-					// 		</span>
-					// 	</li>);
-					// }
-					// else if (wrongUndock.indexOf(k) >= 0) {
-					// 	centerCol.push(<li key={k} style={style} className={"wrongUndockCont"}>
-					// 		<span className="wrongUndock left">{this.props.displayBinId ? k : null}</span>
-					// 	</li>);
-					// }
-					// else if (selectedBin.indexOf(k) >= 0) {
-					// 	centerCol.push(<li key={k} style={style} className={"selectedbinCont"}>
-					// 		<span className="selectedbin">{this.props.displayBinId ? k : null}</span>
-					// 	</li>);
-					// }
-					
-					// else {
-						centerCol.push(<li key={k} style={{"backgroundColor": binColors[k], ...style}}><span>{this.props.displayBinId ? k : null}</span></li>);
-					//}
-
+					centerCol.push(<li key={k} onClick={this.onMtuSelect.bind(this, k)} style={{"backgroundColor": binColors[k], ...style}}><span>{this.props.displayBinId ? k : null}</span></li>);
 				}
-
 			}
 		}
 		
