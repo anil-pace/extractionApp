c7a95c15d738783f2c72f27f02279b494c13afe9
Anil kumar <anil.k@greyorange.com>
2020-05-01 11:39:32 +0530

    BSS-22115: fix updated

--- src/assets/js/constants/configConstants.js
+++ src/assets/js/constants/configConstants.js
@@ -1,7 +1,7 @@
 var configConstants = {
-    WEBSOCKET_IP: 'ws://localhost:8080',
-    INTERFACE_IP: 'https://localhost',
-    PLATFORM_IP: 'http://localhost:8080'
+    WEBSOCKET_IP: 'ws://192.168.8.193:8080',
+    INTERFACE_IP: 'https://192.168.8.193',
+    PLATFORM_IP: 'http://192.168.8.193:8080'
  };
  var BOI_CONFIG =
     configConstants.INTERFACE_IP + '/api/components/get_boi_config';
