c5e4912b5469755d20616beff9a3c601bde7209c
Anil kumar <anil.k@greyorange.com>
2020-05-05 11:28:32 +0530

    BSS-23583: ptalform urls changed to https via nginx

--- src/assets/js/constants/configConstants.js
+++ src/assets/js/constants/configConstants.js
@@ -1,7 +1,7 @@
 var configConstants = {
     WEBSOCKET_IP: 'ws://192.168.8.193:8080',
     INTERFACE_IP: 'https://192.168.8.193',
-    PLATFORM_IP: 'http://192.168.8.193:8080'
+    PLATFORM_IP: 'https://192.168.8.193/api'
  };
  var BOI_CONFIG =
     configConstants.INTERFACE_IP + '/api/components/get_boi_config';
