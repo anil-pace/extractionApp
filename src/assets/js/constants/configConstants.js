f2aa391571c42b56922b438acaff61451fb67312
Anil kumar <anil.k@greyorange.com>
2020-05-07 00:03:12 +0530

    BSS-22115: ws redirection via nginx

--- src/assets/js/constants/configConstants.js
+++ src/assets/js/constants/configConstants.js
@@ -1,10 +1,9 @@
 var configConstants = {
-    WEBSOCKET_IP: 'ws://192.168.8.193:8080',
-    INTERFACE_IP: 'https://192.168.8.193',
-    PLATFORM_IP: 'https://192.168.8.193/api'
- };
- var BOI_CONFIG =
-    configConstants.INTERFACE_IP + '/api/components/get_boi_config';
- configConstants['BOI_CONFIG'] = BOI_CONFIG;
- module.exports = configConstants;
- 
+   WEBSOCKET_IP: 'wss://localhost',
+   INTERFACE_IP: 'https://localhost',
+   PLATFORM_IP: 'https://localhost/api'
+};
+var BOI_CONFIG =
+   configConstants.INTERFACE_IP + '/api/components/get_boi_config';
+configConstants['BOI_CONFIG'] = BOI_CONFIG;
+module.exports = configConstants;
