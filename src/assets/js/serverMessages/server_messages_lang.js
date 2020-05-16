b8542045c45ea2a6e5e06828c88e8a6023a1a152
Anil kumar <anil.k@greyorange.com>
2020-05-16 12:54:44 +0530

    BSS-23778: server_messages_lang.js updated

--- src/assets/js/serverMessages/server_messages_lang.js
+++ src/assets/js/serverMessages/server_messages_lang.js
@@ -4,32 +4,22 @@
  */
 function server_messages_list() {
   
-  _("User Name")
-  _("Password")
-  _("Connection is closed. Connecting...")
-  _("Slot scan successful")
-  _("Cancel scan successful")
-  _("Finish")
-  _("OK")
-  _("FINISH")
-  _("TOTE")
-  _("System is Idle")
-  _("CURRENT SLOT")
-  _("Logout")
-  _("Login")
-  _("Enter Username")
-  _("Enter Password")
-  _("Invalid credentials")
-  _("Login not allowed. You're already logged in")
-  _("Slot scan successful")
-  _("LOGIN")
-  _("Language")
   _("Username")
+  _("Password")
+  _("Language")
   _("Login to")
   _("Enter username")
   _("Enter password")
+  _("LOGIN")
+  _("Logout")
   _("Scan ID card to login.")
 
+  _("Connection is closed. Connecting...")
+  _("System is Idle")
+  _("Invalid credentials")
+  _("Login not allowed. You're already logged in")
+  _("Slot scan successful")
+  _("Cancel scan successful")
 
   _("Wait for MTU")
   _("Select MTU point")
@@ -50,5 +40,4 @@ function server_messages_list() {
   _("All Entities removed")
   _("Are you sure all Entities are removed from the MTU?")
   _("MTU will auto undock after Confirmation.")
-  
 }
