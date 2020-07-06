0421475b1be34defe2c2bc8df61b8fbc0907736d
Jenkins <jenkins@greyorange.com>
2020-07-06 19:51:44 +0000

    BSS-6765 Commit from Jenkins Job

--- src/assets/js/serverMessages/chinese.js
+++ src/assets/js/serverMessages/chinese.js
@@ -1,3 +1,1 @@
-var obj = {
-};
-module.exports = obj;
+var obj={"Username":"Username_chinese","Password":"Password_chinese","Language":"Lanuage_Chinese","Enter username":"Enter username_Chinese","Enter password":"Enter password_Chinese","new text introduced":""};module.exports=obj
