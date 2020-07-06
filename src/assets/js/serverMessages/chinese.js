fd204d5cbaf5ab24bc8680012acc34f39f09c77c
Anil kumar <anil.k@greyorange.com>
2020-07-07 01:28:05 +0530

    BSS-12345: fixed

--- src/assets/js/serverMessages/chinese.js
+++ src/assets/js/serverMessages/chinese.js
@@ -1,1 +1,9 @@
-var obj={"Username":"Username_chinese","Password":"Password_chinese","Language":"Lanuage_Chinese","Enter username":"Enter username_Chinese","Enter password":"Enter password_Chinese","new text introduced":""};module.exports=obj
+var obj = {
+  Username: "Username_chinese",
+  Password: "Password_chinese",
+  Language: "Lanuage_Chinese",
+  "Enter username": "Enter username_Chinese",
+  "Enter password": "Enter password_Chinese",
+  "new text introduced": "",
+};
+module.exports = obj;
